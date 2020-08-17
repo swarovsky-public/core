f2209a0aae1d574b8dff892747a51e84744a3aaf
Gianluca Bragazzi <swarovsky@queendomofhera.com>
2020-08-17 18:54:30 +0200

    v 1.0.0-alpha

--- src/resources/views/admin/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-
-@section('content')
-<div class="uk-section-small">
-    <div class="uk-container uk-container-large">
-        <h1>Admin dashboard here</h1>
-    </div>
-</div>
-@endsection
