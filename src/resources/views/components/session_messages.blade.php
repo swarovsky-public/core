ed9e4b009aa26566639ab5596b5853cf6afde2a2
Gianluca Bragazzi <swarovsky@queendomofhera.com>
2020-08-22 10:02:17 +0200

    Various fixes

--- src/resources/views/components/session_messages.blade.php
+++ src/resources/views/components/session_messages.blade.php
@@ -1,22 +1,24 @@
 <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+<script>
+    let notyf = new Notyf({
+        duration: 4000,
+        dismissible: true,
+        types: [
+            {
+                type: 'warning',
+                background: 'orange',
+                icon: {
+                    className: 'material-icons',
+                    tagName: 'i',
+                    text: 'warning'
+                }
+            },
+        ]
+    });
+</script>
 @if ($errors->any())
     <script>
-        let notyf = new Notyf({
-            duration: 4000,
-            dismissible: true,
-            types: [
-                {
-                    type: 'warning',
-                    background: 'orange',
-                    icon: {
-                        className: 'material-icons',
-                        tagName: 'i',
-                        text: 'warning'
-                    }
-                },
-            ]
-        });
         @foreach ($errors->get('success') as $error)
             setTimeout(function () {
                 notyf.success("{{$error}}");
